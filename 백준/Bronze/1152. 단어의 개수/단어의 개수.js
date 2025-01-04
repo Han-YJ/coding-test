7252044fa7aea26612b7275e757ac9c220e8edc8
han <59593742+Han-YJ@users.noreply.github.com>
2025-01-04 21:38:19 +0900

    [Bronze II] Title: 단어의 개수, Time: 196 ms, Memory: 33172 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const filePath =
+  process.platform === 'linux' ? 'dev/stdin' : '../../example.txt';
+const word = fs.readFileSync(filePath).toString().trim();
+console.log(word.split(' ').filter((s) => s !== '').length);
