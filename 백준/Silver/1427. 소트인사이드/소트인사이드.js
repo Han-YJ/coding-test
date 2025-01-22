700405367af1860bd30f95d7092f681ebaa8da76
han <59593742+Han-YJ@users.noreply.github.com>
2025-01-22 16:22:14 +0900

    [Silver V] Title: 소트인사이드, Time: 96 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const filePath =
+  process.platform === 'linux' ? 'dev/stdin' : '../../example.txt';
+const input = fs.readFileSync(filePath).toString().trim();
+
+console.log(
+  input
+    .split('')
+    .map(Number)
+    .sort((a, b) => b - a)
+    .join('')
+);
