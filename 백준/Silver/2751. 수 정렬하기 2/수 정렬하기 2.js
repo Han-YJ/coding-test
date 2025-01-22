adb8394c890d8c9d34592a7fca1c756529644431
han <59593742+Han-YJ@users.noreply.github.com>
2025-01-22 15:56:07 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1256 ms, Memory: 245788 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const filePath =
+  process.platform === 'linux' ? 'dev/stdin' : '../../example.txt';
+const [count, ...numbers] = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+console.log(numbers.sort((a, b) => a - b).join('\n'));
