1c0b2ec9fc6829e835232c104936f38a5a713358
han <59593742+Han-YJ@users.noreply.github.com>
2025-04-07 12:58:16 +0900

    [Bronze IV] Title: 베라의 패션, Time: 88 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const filePath =
+  process.platform === 'linux' ? 'dev/stdin' : '../../example.txt';
+const input = fs.readFileSync(filePath).toString().trim();
+const N = Number(input);
+
+console.log(Math.pow(N, 2) - N);
