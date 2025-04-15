e239ac74f831f3636ce30411061520a5c1ff9491
han <59593742+Han-YJ@users.noreply.github.com>
2025-04-15 19:40:16 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.05 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,8 +1,7 @@
 function solution(numbers) {
-    const testStr = numbers.join('')
     let answer = 0
     for (let i = 1; i<=9; i++) {
-        if(!testStr.includes(i)) answer += i
+        if(!numbers.includes(i)) answer += i
     }    
     return answer;
 }
